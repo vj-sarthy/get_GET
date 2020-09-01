02ec7cdbbd93ce6fef729198d697e5c9024ef162
Vijay Sarthy <vsreedh@g.clemson.edu>
2020-09-01 14:06:48 -0700

    Updating copyright headers and author information

--- gas_exchange_threshold.py
+++ gas_exchange_threshold.py
@@ -1,15 +1,48 @@
-#Python code to determine the Gas Exchange Threshold (GET) from an incremental ramp test.
-#Vijay Sarthy, Ph.D., Mechanical Engineering, Clemson University, USA
+"""
+BSD 3-Clause License
 
-#Preparing the data, Please make sure that:
-#1. Time is on column 1 and is expressed in multiples of 10 and not as 0:10, 0:20, and so on...
-#2. vo2 data is on column 2 and vco2 data is on column 3 in the approapriate excel file.
-#Eg: If your sheet is named as Iron Man, please type 'Iron Man' instead of 'Sheet4'
+Copyright (c) 2020, vj-sarthy
+All rights reserved.
 
-# copyright
-# author
-# email, contact
-# license
+@Author: Vijay Sarthy, Ph.D., Mechanical Engineering, Clemson University, USA
+@Email: vsreedh@g.clemson.edu
+
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.'
+
+
+Python code to determine the Gas Exchange Threshold (GET) from an cardiopulmonary exercise test.
+
+Preparing the data, Please make sure that:
+1. Time is on column 1 and is expressed in multiples of 10 and not as 0:10, 0:20, and so on...
+2. vo2 data is on column 2 and vco2 data is on column 3 in the approapriate excel file.
+Eg: If your sheet is named as Iron Man, please type 'Iron Man' instead of 'Sheet4'
+
+
+"""
 
 import os
 import click
